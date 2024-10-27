39cb2514d0baa303959f8e57b6f6401c218c15bb
dinesh kumar <dineshvel007@gmail.com>
2024-10-27 16:50:17 +0530

    init

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,15 +1,16 @@
 importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
+
+
 const firebaseConfig = {
-  apiKey: "",
-  authDomain:  "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyCLDuQGn0HlAP3s2pSm6_HD57LaPfV8zZc",
+  authDomain:  "fcmangular-e677c.firebaseapp.com",
+  projectId: "fcmangular-e677c",
+  storageBucket: "fcmangular-e677c.appspot.com",
+  messagingSenderId: "162187589413",
+  appId: "1:162187589413:web:6bf8c04d048a03e3844ccc",
+  measurementId: "G-Y016HYV7X7"
   };
 
 firebase.initializeApp(firebaseConfig);
