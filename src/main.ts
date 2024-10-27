030b1479f7151d51e705dd0fdcfb01ef13ccafaa
dinesh kumar <dineshvel007@gmail.com>
2024-10-27 19:43:14 +0530

    init

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/firebase-messaging-sw.js')
+      navigator.serviceWorker.register('/fcmangular/firebase-messaging-sw.js')
         .then(registration => {
           console.log('Service Worker registered with scope:', registration.scope);
         })
