aa26ccddc98243e258a50b08330ab6910bdab30c
dinesh kumar <dineshvel007@gmail.com>
2024-10-27 16:23:54 +0530

    ini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from './firebase.service';
+import { NotificationPayload } from '@firebase/messaging';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'fcmangular';
+export class AppComponent implements OnInit{
+  notifications: NotificationPayload[] = [];
+  constructor(private firebase: FirebaseService) { }
+
+  ngOnInit() {
+    this.firebase.notifications.subscribe((payload: NotificationPayload) => {
+      if(payload.body) this.notifications.push(payload);
+    });
+  }
 }
---
+++ src/app/firebase.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { NotificationPayload, getMessaging, getToken, onMessage } from "firebase/messaging";
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  firebaseConfig = {
+    apiKey: "AIzaSyCLDuQGn0HlAP3s2pSm6_HD57LaPfV8zZc",
+    authDomain: "fcmangular-e677c.firebaseapp.com",
+    projectId: "fcmangular-e677c",
+    storageBucket: "fcmangular-e677c.appspot.com",
+    messagingSenderId: "162187589413",
+    appId: "1:162187589413:web:6bf8c04d048a03e3844ccc",
+    measurementId: "G-Y016HYV7X7",
+  };
+ // VAPID_KEY = "<ADD_PUBLIC_KEY_FROM_FIREBASE_WEB_PUSH_CERTIFICATES>";
+ VAPID_KEY="BElrLY218GtVXTFSfejpmdggDtTuhVK3SpAynHdJrqXYGMBkZSgs9jRM9isZ8RTh7WICWQl9hMHkpqq9Bx_to64";
+  notifications = new BehaviorSubject<NotificationPayload>({});
+
+  app = initializeApp(this.firebaseConfig);
+  messaging = getMessaging(this.app);
+  constructor() { 
+    this.getFcmToken();
+    this.setupMessageHandler();
+  }
+
+  async getFcmToken() {
+    const fcmToken = await getToken(this.messaging, { vapidKey: this.VAPID_KEY });
+    if (fcmToken) {
+      console.log(fcmToken);
+    }
+  }
+
+  setupMessageHandler() {
+    onMessage(this.messaging, (payload) => {
+      if (payload.notification) {
+        this.notifications.next(payload.notification);
+      }
+    });
+  };
+}
