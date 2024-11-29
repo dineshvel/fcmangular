3ff409480f9398b817aebca060904b4b38864032
dineshvel <dineshvel007@gmail.com>
2024-11-29 14:39:01 +0530

    Update firebase.service.ts

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -8,17 +8,8 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class FirebaseService {
  tokenn:string='';
-  firebaseConfig = {
-    apiKey: "AIzaSyCLDuQGn0HlAP3s2pSm6_HD57LaPfV8zZc",
-    authDomain: "fcmangular-e677c.firebaseapp.com",
-    projectId: "fcmangular-e677c",
-    storageBucket: "fcmangular-e677c.appspot.com",
-    messagingSenderId: "162187589413",
-    appId: "1:162187589413:web:6bf8c04d048a03e3844ccc",
-    measurementId: "G-Y016HYV7X7",
-  };
+ 
  // VAPID_KEY = "<ADD_PUBLIC_KEY_FROM_FIREBASE_WEB_PUSH_CERTIFICATES>";
- VAPID_KEY="BElrLY218GtVXTFSfejpmdggDtTuhVK3SpAynHdJrqXYGMBkZSgs9jRM9isZ8RTh7WICWQl9hMHkpqq9Bx_to64";
   notifications = new BehaviorSubject<NotificationPayload>({});
 
   app = initializeApp(this.firebaseConfig);
